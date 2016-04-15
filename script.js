d5b573a747089ab1c21bd73b9b4a2cbb3cd644b7
DMitryShutov <7dmitry77@gmail.com>
2016-04-15 16:07:00 +0400

    Responsive version

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 window.onload = function () {
     var hamburger = document.querySelector('.lineWrap');
     var navigation = document.querySelector('.nav');
+    var form = document.querySelector('.slider_form')
     hamburger.addEventListener('click', function () {
        navigation.classList.toggle("openMenu");
-       hamburger.classList.toggle("closeButton");     
+       hamburger.classList.toggle("closeButton");
+       form.classList.toggle("push_form");  
     });
     
     
